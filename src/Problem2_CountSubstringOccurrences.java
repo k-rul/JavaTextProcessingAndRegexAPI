c7185b7eef8f225fead12cbe8fe45d096774fdbf
momchil-iliev <momchil.s.iliev@gmail.com>
2016-04-04 21:52:25 +0300

    All Problems With bug on Problem 1

--- src/Problem2_CountSubstringOccurrences.java
+++ src/Problem2_CountSubstringOccurrences.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Problem2_CountSubstringOccurrences {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-
         String text = in.nextLine().toLowerCase();
-        String s = in.nextLine();
+        String find = in.nextLine().toLowerCase();
+        int lengthWord = find.length();
 
-        String rep ="["+ s + "]+";
-
-        Pattern pattern = Pattern.compile(rep);
-        Matcher matcher = pattern.matcher(text);
         int count = 0;
-
-        while (matcher.find()) {
-            matcher.group();
-            count++;
+        for (int i = 0; i <= text.length() - lengthWord; i++) {
+            if (find.equals(text.substring(i, i + lengthWord))) {
+                count++;
+            }
         }
         System.out.println(count);
-
     }
 }
--- src/Problem5_ExtractWords.java
+++ src/Problem5_ExtractWords.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Problem1_ExtractEmails {
+public class Problem5_ExtractWords {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         String text = in.nextLine();
-        String p = "([0-9a-zA-Z]+[0-9a-zA-Z(\\.\\-_)]+[0-9a-zA-Z])@[0-9a-zA-Z]+(.)[a-zA-Z]+\\.[0-9a-zA-Z]+";
+        String p = "[a-zA-Z]+";
 
         Pattern pattern = Pattern.compile(p);
         Matcher matcher = pattern.matcher(text);
 
-        while (matcher.find()) {
-            System.out.println(matcher.group());
+        while (matcher.find()){
+            System.out.printf("%s ",matcher.group());
         }
     }
 }
